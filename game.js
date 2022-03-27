1434840997e119f1f0d60fa8d535015d58eeebc4
ombaldan <ombaldan@gmail.com>
2022-03-27 23:38:29 +0800

    semangat mencari cinta

--- game.js
+++ game.js
@@ -1,16 +1,55 @@
 
-class Player{
-    constructor(name, playerId){
-        this.name = name
-        this.playerId = playerId
-    }
+function computerChoices() {
+    let computer = Math.random();
 
-    randomScore(){
-        let score = Math.floor(Math.random() * 100)
-
-        let scoreElement = document.getElementById(this.elementId)
-        scoreElement.textContent = score
-
-        this.score = score
+    if( computer < 0.34 ) {
+        computer = "gunting";
+    } else if( computer >= 0.34 && computer < 0.67 ) {
+        computer = "batu";
+    } else {
+        computer = "kertas";
     }
 }
+
+function result(computer, player){
+    if( player == "gunting" ){
+        result = (computer == "kertas")
+        setWinner("YOU WINNER");
+    }else if( player == "gunting"){
+        result = (computer == "batu")
+        setWinner("YOU LOSE");
+    }else if( player == "batu"){
+        result = (computer == "gunting")
+        setWinner("YOU WIN");
+    }else if( player == "batu"){
+        result = (computer == "kertas")
+        setWinner("YOU LOSE");
+    }else if( player == "kertas"){
+        result = (computer == "gunting")
+        setWinner("YOU LOSE");
+    }else if( player == "kertas"){
+        result = (computer == "batu")
+        setWinner("YOU WIN");
+    }else{
+        setWinner("SERI");
+    }
+}
+
+function setWinner(text){
+    let winner = document.getElementById("the-winner")
+    winner.textContent = text
+}
+
+let playerGunting = document.getElementById("player-gunting");
+playerGunting.addEventListener("click", function(){
+    let pilihanComputer = computerChoices();
+    let pilihanPlayer = playerGunting.;
+    let result = result(pilihanComputer, pilihanPlayer);
+    // console.log("computer:" + pilihanComputer);
+    // console.log("player:" + pilihanPlayer);
+    // console.log("result:" + result);
+});
+
+function reset(){
+    window.location.reload();
+} 
