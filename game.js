259e0e870bb5cbee689c358a59fdd8d0f06fada6
ombaldan <ombaldan@gmail.com>
2022-03-29 17:01:31 +0800

    edit js

--- game.js
+++ game.js
@@ -1,69 +1,106 @@
+$(document).ready(function(){
 
-//ini karena nyontek di Channel Web Pemrograman UNPAS
-// gak tau kenapa dibuat seperti ini
-//udah nyontek gak jalan juga codenya hhahaha
 
-function computerChoices() {
-    let computer = Math.random();
+    $("#plgunting").click(function(){
+      let pGunting = 'gunting';
+      let player = "";
+      let winner = document.getElementById("the-winner");
+      let computer = Math.random();
 
-    if( computer < 0.34 ) {
-        computer = "gunting";
-    } else if( computer >= 0.34 && computer < 0.67 ) {
-        computer = "batu";
-    } else {
-        computer = "kertas";
-    }
+      if( computer < 0.34 ) {
+          computer = "gunting";
+          winner.textContent = "Seri";
+      } else if( computer >= 0.34 && computer < 0.67 ) {
+          computer = "batu";
+          winner.textContent = "Kalah";
+      } else {
+          computer = "kertas";
+          winner.textContent = "Menang";
+      }
+    })
+
+    $("#player-batu").click(function(){
+      let pGunting = 'batu';
+      let player = "";
+      let winner = document.getElementById("the-winner");
+      let computer = Math.random();
+
+      if( computer < 0.34 ) {
+          computer = "gunting";
+          winner.textContent = "Menang";
+      } else if( computer >= 0.34 && computer < 0.67 ) {
+          computer = "batu";
+          winner.textContent = "Seri";
+      } else {
+          computer = "kertas";
+          winner.textContent = "Kalah";
+      }
+    })
+
+    $("#player-kertas").click(function(){
+        let pGunting = 'kertas';
+        let player = "";
+        let winner = document.getElementById("the-winner");
+        let computer = Math.random();
+  
+        if( computer < 0.34 ) {
+            computer = "gunting";
+            winner.textContent = "Kalah";
+        } else if( computer >= 0.34 && computer < 0.67 ) {
+            computer = "batu";
+            winner.textContent = "Menang";
+        } else {
+            computer = "kertas";
+            winner.textContent = "Seri";
+        }
+      })
+  })
+
+  function reset(){
+  window.location.reload();
 }
+  
+  
 
-//masih bingung di sini
-//pengen ganti tulisan "VS" jadi text dari setWinner saat keluar result, tapi gak berfungsi
+  function computerChoices() {
+      let computer = Math.random();
 
-function result(computer, player){
-    if( player == "gunting" ){
-        result = (computer == "kertas")
-        setWinner("YOU WINNER");
-    }else if( player == "gunting"){
-        result = (computer == "batu")
-        setWinner("YOU LOSE");
-    }else if( player == "batu"){
-        result = (computer == "gunting")
-        setWinner("YOU WIN");
-    }else if( player == "batu"){
-        result = (computer == "kertas")
-        setWinner("YOU LOSE");
-    }else if( player == "kertas"){
-        result = (computer == "gunting")
-        setWinner("YOU LOSE");
-    }else if( player == "kertas"){
-        result = (computer == "batu")
-        setWinner("YOU WIN");
-    }else{
-        setWinner("SERI");
-    }
-}
+      if( computer < 0.34 ) {
+          computer = "gunting";
 
-//the winner
-function setWinner(text){
+      } else if( computer >= 0.34 && computer < 0.67 ) {
+          computer = "batu";
+      } else {
+          computer = "kertas";
+      }
+  }
+
+  function setWinner(text){
     let winner = document.getElementById("the-winner")
     winner.textContent = text
 }
 
-//ini juga copas dari web programming unpas, tapi gak bisa running karena element yang dipakai di videonya class, sementara di element htmlnya saya pakai id dan saya bingung manggil id-nya di baris 55 mengganti className.
-let playerGunting = document.getElementById("player-gunting");
-playerGunting.addEventListener("click", function(){
-    let pilihanComputer = computerChoices();
-    let pilihanPlayer = playerGunting.className;
-    let result = result(pilihanComputer, pilihanPlayer);
-    // console.log("computer:" + pilihanComputer);
-    // console.log("player:" + pilihanPlayer);
-    // console.log("result:" + result);
-});
-
-//ini tombol reloadpage
-function reset(){
-    window.location.reload();
-} 
-
-//secara umum untuk chapter 4 pemahaman materi saya cuma 20% "maybe". Sudah beberapa kali replay recording FGD, dan beberapa video referensi lain, tapi masih belum paham menulis kembali ke dalam code. Hehe..
-
-//Belum tahu bagaimana metode belajar yang pas biar mudah memahami rules javascript ini.
+  function result(computer, player){
+      
+      if( player == "gunting" ){
+          result = (computer == "kertas")
+          setWinner("YOU WINNER");
+      }else if( player == "gunting"){
+          result = (computer == "batu")
+          setWinner("YOU LOSE");
+      }else if( player == "batu"){
+          result = (computer == "gunting")
+          setWinner("YOU WIN");
+      }else if( player == "batu"){
+          result = (computer == "kertas")
+          setWinner("YOU LOSE");
+      }else if( player == "kertas"){
+          result = (computer == "gunting")
+          setWinner("YOU LOSE");
+      }else if( player == "kertas"){
+          result = (computer == "batu")
+          setWinner("YOU WIN");
+      }else{
+          setWinner("SERI");
+      }
+  }
